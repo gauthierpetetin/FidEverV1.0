20b1a3b3a8549a71ed4c97718a03097c3b512734
Gauthier Petetin <gauthierpetetin@MacBook-Pro-de-Gauthier.local>
2017-10-14 08:55:51 +0200

    Commit 2

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -2,14 +2,24 @@ import { Component } from '@angular/core';
 
 import { NavController } from 'ionic-angular';
 
+import { AuthProvider } from '../../providers/auth/auth';
+
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
-    
+  constructor(
+    public navCtrl: NavController,
+    public authProvider: AuthProvider) {
+
+  }
+
+  logOut(): void {
+    this.authProvider.logoutUser().then(() => {
+      this.navCtrl.setRoot('LoginPage');
+    });
   }
 
 }
